cb58929b9ac218e8e6b72248fe03e1d83c8b0940
Kunal Joshi <kunaljoshi@gmail.com>
2023-10-06 15:19:42 +0530

    my vercel1 commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python 3.11 -m pip install -r requirement.txt
-python 3.11 manage.py collectstatic --noinput --clear
+python 3.11.5 -m pip install -r requirement.txt
+python 3.11.5 manage.py collectstatic --noinput --clear
 echo "BUILD END"
